f3ba8a1c73c4e3b1c3ea5bd30c0be43c0455c8d3
ankush-dhiman <31234683+ankush-dhiman@users.noreply.github.com>
2017-08-27 22:48:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,31 +7,9 @@ var marginLeft = 0;
 var marginRight = 0;
 function moveRight() {
    
-   if((marginLeft>=0)  && (marginLeft<=1000))
-  {  marginLeft = marginLeft + 10;
+    marginLeft = marginLeft + 10;
     img.style.marginLeft = marginLeft + 'px' ;
-    }
-    
-    else
-    {
-       moveLeft(); 
-    }
-
-}
-
-function moveLeft() {
-  if(marginLeft<0)
-  {
-      moveRight();
-      
-  }
-    
-    else{
-        marginLeft = marginLeft -10;
-    img.style.marginLeft = marginLeft + 'px' ;
-    }
-
-}
+}   
 
 img.onclick = function(){
     var interval = setInterval(moveRight , 50);
