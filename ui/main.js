a9be02eba86404b754617129209d9e50116da93a
ankush-dhiman <31234683+ankush-dhiman@users.noreply.github.com>
2017-08-27 20:03:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,11 +5,11 @@ element.innerHTML = 'New Value';
 var img = document.getElementById('madi');
 var marginLeft = 0;
 function moveRight(){
-    marginLeft = marginLeft + 1;
+    marginLeft - marginLeft + 3;
     img.style.marginLeft = marginLeft + 'px' ;
 }
 
 
 img.onclick = function(){
-    var interval = setInterval(moveRight,50);
+    var interval = setInterval(moveRight , 50);
 };
