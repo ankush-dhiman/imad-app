a9b0e23bd45488039bbc3bd00167ad2bd8f49deb
ankush-dhiman <dhimanankush72@gmail.com>
2017-08-27 16:39:17 +0530

    server js updated

--- server.js
+++ server.js
@@ -20,6 +20,7 @@ function createTemplate(data){
         
         <meta naname="viewport" content="width=device-width, initial-scale=1.0" />
         <link href="style.css" rel="stylesheet" />
+
    </head>
 <body>
 	<div class="container" >
