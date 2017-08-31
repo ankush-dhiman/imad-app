4d6bb979630810405788857802f2edfd464c7873
ankush-dhiman <31234683+ankush-dhiman@users.noreply.github.com>
2017-08-31 21:35:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -67,7 +67,7 @@ function createTemplate(data){
      var heading = data.heading;
      var date = data.date;
      var content = data.content;
-     var htmlTemplate =
+     var htmlTemplate =`
   <html>
   <head>
    	<title>
@@ -98,7 +98,7 @@ function createTemplate(data){
 </body>
 </html>
 
-;
+`;
 return htmlTemplate;
      
 }
