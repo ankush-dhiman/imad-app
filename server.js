c8d4e65e82469a56528b9713e452acddba2b89e5
ankush-dhiman <31234683+ankush-dhiman@users.noreply.github.com>
2017-09-02 15:31:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -127,6 +127,18 @@ app.get('/test-db', function (req, res) {
   });
 });
 
+app.get('/test', function (req, res) {
+pool.query('SELECT *FROM test',function(err,result){
+if(err)
+{
+alert('error');
+}else{
+res.send(JSON.stringify(result.rows));
+}
+});
+
+});
+
 
 
 var counter = 0;
