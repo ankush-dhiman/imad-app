9d13a8579a0befa10bb2f0eee2c88be602f04538
ankush-dhiman <31234683+ankush-dhiman@users.noreply.github.com>
2017-09-03 00:22:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,32 +1,5 @@
-//Counter code
 
-var button = document.getElementById('counter');
-
-button.onclick = function () {
-    
-    //Create a request object
-    var request = new XMLHttpRequest();
-    
-    //Capture response and store it in a variable
-    request.onreadystatechange = function () {
-        //Take some actions
-        if(request.readyState === XMLHttpRequest.DONE)
-        {
-            if(request.status===200)
-            {
-                var counter = request.responseText;
-                var span = document.getElementById('count');
-                span.innerHTML = counter.toString();
-            }
-        }        
-        
-    };
-    
-    request.open('GET' ,'http://dhimanankush72.imad.hasura-app.io/counter' , true );
-    request.send(null);
-} ;
-
-
+//Submit username/password
 var submit = document.getElementById('submit_btn');
 submit.onclick = function () {
     
@@ -42,24 +15,26 @@ submit.onclick = function () {
         {
             if(request.status===200)
             {
-                 //capture a list of names and render it as list
-                var names = request.responseText;
-                names = JSON.parse(names); //convert from string to back in objects
-                var list = '';
+                console.log('user logged in');
+                aler('logged in successfully');
                 
-                for(var i=0 ; i<names.length;i++){
-                    
-                    list += '<li>' + names[i] + '</li>' ;
-                }
-                var ul = document.getElementById('namelist')
-                ul.innerHTML = list;
+            }else if (request.status===403)
+            {
+                alert('username/password is incorrect');
+            }else if (request.status===400)
+            {
+                alert('something went wrong on server');
             }
+            
         }        
         
     };
-    var nameInput = document.getElementById('name');
-    var name = nameInput.value;
-    request.open('GET' ,'http://dhimanankush72.imad.hasura-app.io/submit-name?name=' + name , true );
-    request.send(null);
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    console.log(username);
+    console.log(password);
+    request.open('POST' ,'http://dhimanankush72.imad.hasura-app.io/login' , true );
+    request.setRequestHeader('Content-Type', 'application/json');
+    request.send(JSON.stringify({username: username, password: password}));
     
 };
