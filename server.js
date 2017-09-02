35aeb12ad98584f5df05583266059729d5774c30
ankush-dhiman <31234683+ankush-dhiman@users.noreply.github.com>
2017-09-02 15:40:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -110,7 +110,20 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+
+
 var pool = new Pool(config);
+
+app.get('/test', function (req, res) {
+pool.query('SELECT *FROM test',function(err,result){
+if(err)
+{
+alert('error');
+}else{
+res.send(JSON.stringify(result.rows));
+}
+});
+});
 
 app.get('/test-db', function (req, res) {
   //make a select request
