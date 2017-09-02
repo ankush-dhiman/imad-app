27392b54ad10bc0e799fe5df469de058610001b9
ankush-dhiman <31234683+ankush-dhiman@users.noreply.github.com>
2017-09-02 22:16:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -139,7 +139,7 @@ app.post('/create-user', function (req, res) {
           res.status(500).send(err.toString());
       } else {
           
-          res.send('User succesfully created', username);
+          res.send('User succesfully created'+ username);
       }
         
         
