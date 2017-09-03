32790322b06a9a6871d0ef0bc77354fb84c80238
ankush-dhiman <31234683+ankush-dhiman@users.noreply.github.com>
2017-09-03 22:47:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -157,7 +157,7 @@ app.post('/create-user', function (req, res) {
     });
 });
 
-app.post('/login', function (req, res) {
+/*app.post('/login', function (req, res) {
     
     var username = req.body.username;
     var password = req.body.password;
@@ -196,6 +196,38 @@ app.post('/login', function (req, res) {
         
     });
 });
+*/
+
+app.post('/login',function(res,req){
+var username = req.body.username;
+var password = req.body.password;
+
+pool.query('SELECT * FROM "user" WHERE username = $1', [username], function(err,result){
+     if(err){
+      res.status(500).send(err.toString());
+    }else{
+        if(result.rows.length === 0){
+            res.send(403).send('username/password is invalid');
+        }
+        else{
+            var dbString = result.rows[0].password;
+            var salt = dbString.split('$')[2];
+            var hashedPassword = hash(password, salt);
+            if(hashedPassword === dbString){
+                res.send('credentials correct!');
+            }
+            else{
+                res.send(403).send('username/password is invalid');
+            }
+
+        }
+
+    }
+});
+});
+
+
+
 
 app.get('/check-login', function (req, res) {
     if(req.session && req.session.auth && req.session.auth.userId)
