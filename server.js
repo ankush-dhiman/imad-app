37e4bc28d277240acf3b18873674efab78530165
ankush-dhiman <31234683+ankush-dhiman@users.noreply.github.com>
2017-09-04 14:13:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -91,11 +91,18 @@ function createTemplate(data){
 
    </head>
 <body>
-	<div class="container" >
-            <div class = "navbar" >
-                <a href="/" > Home</a>
+            <div class="container" >
+                                <form class="navbar-form navbar-left">
+                      <div class="input-group">
+                        <input type="text" class="form-control" placeholder="Search">
+                        <div class="input-group-btn">
+                          <button class="btn btn-default" type="submit">
+                            <i class="glyphicon glyphicon-search"></i>
+                          </button>
+                        </div>
+                      </div>
+              </form>
                 
-            </div> 
              <hr/>
                 <h3> ${heading} </h3>        
             
