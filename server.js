6bd6cafbedd64a3aa9b3ed08a443816ca2fa0018
ankush-dhiman <31234683+ankush-dhiman@users.noreply.github.com>
2017-09-04 14:17:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -270,7 +270,7 @@ app.get('/submit-name', function (req, res) {
   res.send(JSON.stringify(names));
 });
 
-app.get('/:articleName', function (req, res) {
+app.get('/articles/:articleName', function (req, res) {
    
    var articleName = req.params.articleName;
    
