fcd02ab51b17b40d3b094f6a646a1ffa89c09eda
ankush-dhiman <31234683+ankush-dhiman@users.noreply.github.com>
2017-09-05 20:06:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -291,6 +291,9 @@ app.get('/articles/:articleName', function (req, res) {
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
+app.get('/ui/bootstrap/css/bootstrap.min.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui/bootstrap/css', 'bootstrap.min.css'));
+});
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
